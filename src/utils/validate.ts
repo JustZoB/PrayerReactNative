ac12622fc1cf0c943818b170022e598975359ac1
JustZoB <justzofb@gmail.com>
2022-03-31 15:52:54 +0600

    Fix validate. Add trim and validate email

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,8 +1,16 @@
+export const validateEmail = (email: string) => {
+  let re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
 export const loginValidate = (values: { email: string, password: string }) => {
   const errors: { email?: string, password?: string } = {}
 
-  if (!values.email) {
+  if (!values.email || values.email.trim() === '') {
     errors.email = 'Required'
+  }
+  if (!validateEmail(values.email)) {
+    errors.email = 'It is not email'
   }
   if (values.email && values.email.length > 20) {
     errors.email = 'Too long'
@@ -20,13 +28,16 @@ export const loginValidate = (values: { email: string, password: string }) => {
 export const singUpValidate = (values: { email: string, name: string, password: string }) => {
   const errors: { email?: string, name?: string, password?: string } = {}
 
-  if (!values.email) {
+  if (!values.email || values.email.trim() === '') {
     errors.email = 'Required'
+  }
+  if (!validateEmail(values.email)) {
+    errors.email = 'It is not email'
   }
   if (values.email && values.email.length > 20) {
     errors.email = 'Too long'
   }
-  if (!values.name) {
+  if (!values.name.trim()) {
     errors.name = 'Required'
   }
   if (values.name && values.name.length > 20) {
@@ -45,7 +56,7 @@ export const singUpValidate = (values: { email: string, name: string, password: 
 export const columnValidate = (values: { title: string }) => {
   const errors: { title?: string } = {}
 
-  if (!values.title) {
+  if (!values.title || values.title.trim() === '') {
     errors.title = 'Required'
   }
   if (values.title && values.title.length > 20) {
@@ -58,7 +69,7 @@ export const columnValidate = (values: { title: string }) => {
 export const prayerValidate = (values: { title: string }) => {
   const errors: { title?: string } = {}
 
-  if (!values.title) {
+  if (!values.title || values.title.trim() === '') {
     errors.title = 'Required'
   }
   if (values.title && values.title.length > 20) {
@@ -71,7 +82,7 @@ export const prayerValidate = (values: { title: string }) => {
 export const commentValidate = (values: { body: string }) => {
   const errors: { body?: string } = {}
 
-  if (!values.body) {
+  if (!values.body.trim()) {
     errors.body = 'Required'
   }
   if (values.body && values.body.length > 20) {
